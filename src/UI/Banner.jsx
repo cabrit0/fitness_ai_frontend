44681b6df81eb0515507c30b0d03cffdb4d1242f
cabrit0 <cabrit0o.dev@gmail.com>
2022-12-20 23:31:57 +0000

    Banner and menu+drawer responsive

--- src/UI/Banner.jsx
+++ src/UI/Banner.jsx
@@ -1,24 +1,108 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import {
+  HiOutlineUserCircle,
+  HiOutlinePhone,
+  HiOutlineInformationCircle,
+  HiOutlineGlobeAlt,
+  HiMenu,
+} from "react-icons/hi";
+import { HiOutlineBackward } from "react-icons/hi2";
 
 function Banner() {
-  const currentTime = new Date();
-  const currentDay = currentTime.toLocaleString("default", { weekday: "long" });
+  const [time, setTime] = useState(new Date());
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => setTime(new Date()), 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  function toggleMenu() {
+    setIsMenuOpen(!isMenuOpen);
+  }
 
   return (
-    <div className="bg-gray-200 py-4">
+    <div className="bg-gray-800 py-4">
       <div className="container mx-auto px-4">
-        <h1 className="text-4xl font-bold mb-4">Gym Title</h1>
-        <div className="flex justify-between">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-            Login
-          </button>
-          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full">
-            Request Information
-          </button>
+        <div className="flex items-center justify-between">
+          <h1 className="text-2xl font-bold text-gray-100">Gym Title</h1>
+          <div className="hidden md:flex">
+            <div className="flex items-center justify-center font-bold py-2 px-4 mr-4 hover:text-white text-gray-400 focus:outline-none focus:shadow-outline transition-all duration-300 ease-in-out hover:bg-transparent hover:border-transparent">
+              <HiOutlineUserCircle className="mr-2" />
+              Login
+            </div>
+            <div className="flex items-center justify-center font-bold py-2 px-4 mr-4 hover:text-white text-gray-400 focus:outline-none focus:shadow-outline transition-all duration-300 ease-in-out hover:bg-transparent hover:border-transparent">
+              <HiOutlinePhone className="mr-2" />
+              Contact
+            </div>
+            <div className="flex items-center justify-center font-bold py-2 px-4 hover:text-white text-gray-400 focus:outline-none focus:shadow-outline transition-all duration-300 ease-in-out hover:bg-transparent hover:border-transparent">
+              <HiOutlineInformationCircle className="mr-2" />
+              About
+            </div>
+            <div className="flex items-center justify-center font-bold py-2 px-4 ml-4 hover:text-white text-gray-400 focus:outline-none focus:shadow-outline transition-all duration-300 ease-in-out hover:bg-transparent hover:border-transparent">
+              <HiOutlineGlobeAlt className="mr-2" />
+              Website
+            </div>
+          </div>
+          <div className="md:hidden">
+            <button
+              onClick={toggleMenu}
+              className="font-bold py-2 px-4 hover:text-white text-gray-400 focus:outline-none focus:shadow-outline transition-all duration-300 ease-in-out hover:bg-transparent hover:border-transparent"
+            >
+              <HiMenu className="mr-2" />
+              Menu
+            </button>
+          </div>
         </div>
-        <p className="text-xl font-bold mt-4">
-          Today is {currentDay}, {currentTime.toLocaleTimeString()}
-        </p>
+        <div className="text-gray-300 text-sm mt-4 opacity-75">
+          {time.toLocaleDateString()} {time.toLocaleTimeString()}
+        </div>{" "}
+        <div
+          className={`${
+            isMenuOpen ? "block" : "hidden"
+          } md:hidden absolute top-0 right-0 w-${80} h-full bg-gray-800 z-50 transform transition duration-400 ease-in-out`}
+        >
+          <div className="flex items-center justify-between px-4 py-3 bg-gray-700">
+            <h1 className="text-3xl font-bold text-gray-100">Menu</h1>
+            <button
+              onClick={toggleMenu}
+              className="font-bold py-2 px-4 hover:text-white text-gray-400 focus:outline-none focus:shadow-outline transition-all duration-300 ease-in-out hover:bg-transparent hover:border-transparent"
+            >
+              <HiOutlineBackward className="mr-2" />
+              Back
+            </button>
+          </div>
+          <div className="py-4">
+            <div
+              onClick={toggleMenu}
+              className="flex items-center justify-center font-bold py-2 px-4 hover:text-white text-gray-400 focus:outline-none focus:shadow-outline transition-all duration-300 ease-in-out hover:bg-transparent hover:border-transparent"
+            >
+              <HiOutlineUserCircle className="mr-2" />
+              Login
+            </div>
+            <div
+              onClick={toggleMenu}
+              className="flex items-center justify-center font-bold py-2 px-4 hover:text-white text-gray-400 focus:outline-none focus:shadow-outline transition-all duration-300 ease-in-out hover:bg-transparent hover:border-transparent"
+            >
+              <HiOutlinePhone className="mr-2" />
+              Contact
+            </div>
+            <div
+              onClick={toggleMenu}
+              className="flex items-center justify-center font-bold py-2 px-4 hover:text-white text-gray-400 focus:outline-none focus:shadow-outline transition-all duration-300 ease-in-out hover:bg-transparent hover:border-transparent"
+            >
+              <HiOutlineInformationCircle className="mr-2" />
+              About
+            </div>
+            <div
+              onClick={toggleMenu}
+              className="flex items-center justify-center font-bold py-2 px-4 hover:text-white text-gray-400 focus:outline-none focus:shadow-outline transition-all duration-300 ease-in-out hover:bg-transparent hover:border-transparent"
+            >
+              <HiOutlineGlobeAlt className="mr-2" />
+              Website
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,38 +1,13 @@
 import React from "react";
-import ActivityList from "../components/ActivityList";
-import Banner from "../components/Banner";
+import Banner from "../UI/Banner";
 
-function HomePage() {
-  const activities = [
-    { id: 1, name: "Yoga" },
-    { id: 2, name: "Pilates" },
-    { id: 3, name: "Spinning" },
-  ];
-
+const HomePage = () => {
   return (
-    <div className="h-screen flex flex-col bg-black text-white">
+    <div>
       <Banner />
-      <div className="my-8 mx-4 lg:mx-auto max-w-4xl">
-        <h2 className="text-2xl font-bold mb-4 text-center">
-          Welcome to our fitness center!
-        </h2>
-        <p className="text-gray-300 mb-8 text-center">
-          Here at our center, we offer a wide range of activities to help you
-          reach your fitness goals. From yoga and pilates to spinning and more,
-          we have something for everyone.
-        </p>
-        <div className="flex justify-around">
-          <button className="bg-blue-100 hover:bg-blue-200 text-blue-900 font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline">
-            Login
-          </button>
-          <button className="bg-green-100 hover:bg-green-200 text-green-900 font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline">
-            Request Info
-          </button>
-        </div>
-      </div>
-      <ActivityList activities={activities} />
+      <div>HomePage</div>
     </div>
   );
-}
+};
 
 export default HomePage;
