e2d3ba311be07c3071c185a1d1c9635a4bcf0807
cabrit0 <cabrit0o.dev@gmail.com>
2022-12-22 18:48:47 +0000

    text-portugues

--- src/components/GymDescriptionBanner.jsx
+++ src/components/GymDescriptionBanner.jsx
@@ -4,9 +4,10 @@ const GymDescriptionBanner = () => {
   return (
     <div className="bg-gray-800 p-4 rounded-lg mt-5 mx-7">
       <div className="text-center text-gray-300 text-lg mt-4">
-        Our gym is the best in town, with top-of-the-line equipment and
-        knowledgeable trainers to help you reach your fitness goals. We offer a
-        variety of classes and personal training sessions.
+        Nossa academia é a melhor da cidade, com equipamentos de última geração
+        e instrutores experientes para ajudá-lo a alcançar seus objetivos de
+        condicionamento físico. Oferecemos uma variedade de aulas e sessões de
+        treinamento pessoal.
       </div>
       <div className="flex justify-center mt-4">
         <button className="bg-gray-600 py-2 px-4 rounded-full text-white mr-4">
