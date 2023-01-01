1e29296f4f501f309646ec3050eb38c695e9c0ac
cabrit0 <cabrit0o.dev@gmail.com>
2023-01-01 21:52:05 +0000

    fix

--- src/UI/ProfileBanner.jsx
+++ src/UI/ProfileBanner.jsx
@@ -7,18 +7,18 @@ const ProfileBanner = () => {
   const firstLetter = user.foundUser.username[0];
 
   return (
-    <div className="bg-gray-50 m-8 px-12 lg:mx-56 py-5 rounded-3xl opacity-50 shadow-2xl flex justify-between items-center">
-      <div className="rounded-full shadow-lg h-20 w-20 bg-blue-300 flex items-center justify-center">
+    <div className="bg-gray-50  m-8 px-12 lg:mx-56 py-5 rounded-3xl opacity-50 shadow-2xl flex justify-between items-center">
+      <div className="rounded-full shadow-lg h-20 w-20 bg-blue-300 flex items-center justify-center ">
         <p className="text-center my-auto text-stone-600 font-bold text-5xl">
           {firstLetter}
         </p>
       </div>
       <div>
         <div>
-          <p className="font-bold text-stone-900 text-2xl">
+          <p className="font-bold text-stone-900 text-end md:text-2xl text-lg">
             {user.foundUser.username}
           </p>
-          <p className="font-bold text-stone-900 text-lg">
+          <p className="font-bold text-stone-900 text-end md:text-2xl text-lg">
             {user.foundUser.roles}
           </p>
         </div>
